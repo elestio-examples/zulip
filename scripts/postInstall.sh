ffc42e93a7a62ea44308f742924c859b3772fdf4
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-26 18:06:29 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,7 +1,8 @@
-#set env vars
+set env vars
 set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-sleep 30s;
+sleep 150s;
 
+docker-compose exec -T zulip su zulip -c '/home/zulip/deployments/current/manage.py create_realm zulip ${ADMIN_EMAIL} ${ADMIN_EMAIL} --password ${ADMIN_PASSWORD}'
