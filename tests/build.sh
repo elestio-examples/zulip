bc06a8ebcf9e09320fada41f7a1df4e1b29e2e5a
Prem chavhan <pchavhan@logiquad.com>
2023-07-27 13:36:25 +0530

    updated build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
+sed -i "s~ARG UBUNTU_MIRROR~ARG UBUNTU_MIRROR='http://us.archive.ubuntu.com/ubuntu/'~g" Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/zulip:latest | docker load
